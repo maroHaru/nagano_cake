d74b45bde606ca5ce76c34041dc75f084d9fd615
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-07-19 09:42:13 +0000

    [Add] Creating a controller for device

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  
   root to: "homes#top"
   get '/about', to: 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
