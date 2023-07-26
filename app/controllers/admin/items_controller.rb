dfd21758bc16283881d634a94943eb1d8782a794
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-07-26 14:51:11 +0000

    [Add] Partial implementation of Item detail pages

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,6 +13,10 @@ class Admin::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
   end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :genre
+
+  def get_image
+    image
+  end
 end
