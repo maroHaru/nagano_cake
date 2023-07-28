db5f8d85aee9b5e93a39d9ec3893fbd6f51ce2ea
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-07-28 20:40:48 +0000

    [Add]Implementation of customer-side Item list and Item details

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items, except: [:destroy]
   end
+
+  scope module: :public do
+    resources :items, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
