31ec018037dfe96057081c4934eb51303380c4e3
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-07-30 03:00:01 +0000

    [Add]Implementing a  address edit page and a delete function

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,6 +2,7 @@ class Public::AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
     redirect_to addresses_path
   end
@@ -11,9 +12,25 @@ class Public::AddressesController < ApplicationController
     @address = Address.new
   end
 
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
   private
 
   def address_params
-    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end
