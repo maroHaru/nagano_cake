68a98adbdac7670ab4b94c0b26497bac61f092dc
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-08-04 12:32:08 +0000

    [Add] Partial implementation so that the registered address can be selected with radio buttons and select

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,20 +1,23 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
     @order = Order.new
+    @customer = current_customer
+    @customers = Customer.all
+    @addresses = Address.all
   end
-  
+
   def create
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.save
     redirect_to orders_confirm_path
   end
-  
+
   private
-  
+
   def order_params
-    params.require(:order).permit(:payment_method)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_cost, :total_payment)
   end
-  
+
 end
