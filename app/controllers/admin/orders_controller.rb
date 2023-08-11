09ed21618361ee56dbf4352166c46ba37d744e2e
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-08-11 21:14:51 +0000

    [Update]Completed implementation of order status change on admin screen

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,4 +7,17 @@ class Admin::OrdersController < ApplicationController
     # @orders = Order.all
     # @order_detail = OrderDetail.all
   end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order.id)
+  end
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+  
 end
