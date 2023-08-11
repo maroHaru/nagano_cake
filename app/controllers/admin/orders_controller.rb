bcfee7b85636c83db90b47dfc8ae1c6563067f93
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-08-11 19:15:49 +0000

    [Update]Completed implementation of order list and order details on admin screen (other than status function)

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,9 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @orders = Order.all
-    @order_detail = OrderDetail.all
+    @order_detail = OrderDetail.find(params[:id])
+    @order_details = @order.order_details
+    # @orders = Order.all
+    # @order_detail = OrderDetail.all
   end
 end
