df06d422f3d1fc5025186c58271a2975ece988ae
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-08-12 14:08:10 +0000

    [Update]Implementing linkage between order status and production status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,8 +2,20 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_detail = OrderDetail.find(params[:id])
-    @order = @order_detail.order
-    @order_detail.update(order_detail_params)
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
+
+    is_updated = true
+    if @order_detail.update(order_detail_params)
+      @order.update(status: 2) if @order_detail.making_status == "manufacturing"
+
+      @order_details.each do |order_detail|
+        if order_detail.making_status != "finish"
+          is_updated = false
+        end
+      end
+      @order.update(status: 3) if is_updated
+    end
     redirect_to admin_order_path(@order.id)
   end
 
