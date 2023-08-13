eea649099d84cd3ca77ec950654f5ed651217079
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-08-13 03:06:55 +0000

    [Update]making _tatus update completed when order status confirm_payment is changed

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,18 +3,16 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = Order.find(params[:id])
+    @order = @order_detail.order
     @order_details = @order.order_details.all
-
-    is_updated = true
-    if @order_detail.update(order_detail_params)
-      @order.update(status: 2) if @order_detail.making_status == "manufacturing"
-
-      @order_details.each do |order_detail|
-        if order_detail.making_status != "finish"
-          is_updated = false
+    @order_detail.update(order_detail_params)
+    if OrderDetail.find_by(making_status: "manufacturing")
+      @order.update_attribute(:status, "making")
+      if @order_details.each do |order_detail|
+        order_detail.making_status = "finish"
+        @order.update_attribute(:status, "preparing_ship")
         end
       end
-      @order.update(status: 3) if is_updated
     end
     redirect_to admin_order_path(@order.id)
   end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,17 +7,21 @@ class Admin::OrdersController < ApplicationController
     # @orders = Order.all
     # @order_detail = OrderDetail.all
   end
-  
+
   def update
     @order = Order.find(params[:id])
+    @order_detail = OrderDetail.find(params[:id])
     @order.update(order_params)
+    if Order.find_by(status: "confirm_payment")
+      @order_detail.update_attribute(:making_status, "wait_manufacture")
+    end
     redirect_to admin_order_path(@order.id)
   end
-  
+
   private
 
   def order_params
     params.require(:order).permit(:status)
   end
-  
+
 end
