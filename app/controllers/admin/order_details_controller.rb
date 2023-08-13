47231404829e50773c75907e60c44edea3368baf
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-08-13 03:30:48 +0000

    [Update] Linkage between making_status and order status implemented Other than making_status linkage issue

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,16 +3,16 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = Order.find(params[:id])
-    @order = @order_detail.order
+    # @order = @order_detail.order
     @order_details = @order.order_details.all
     @order_detail.update(order_detail_params)
+    if @order_details.each do |order_detail|
+      order_detail.making_status = "finish"
+      @order.update_attribute(:status, "preparing_ship")
+      end
+    end
     if OrderDetail.find_by(making_status: "manufacturing")
       @order.update_attribute(:status, "making")
-      if @order_details.each do |order_detail|
-        order_detail.making_status = "finish"
-        @order.update_attribute(:status, "preparing_ship")
-        end
-      end
     end
     redirect_to admin_order_path(@order.id)
   end
